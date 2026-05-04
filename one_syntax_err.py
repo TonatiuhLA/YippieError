ba626215ef2b5e13deb1f87c106854298669e0ef
Tonatiuh Leung-Arellano <tonatiuhula8@gmail.com>
2026-05-04 18:19:13 -0400

    yippie_err.py detects the first syntax error in a program passed in as python yippie_err.py filename

---
+++ one_syntax_err.py
@@ -0,0 +1,15 @@
+# A program with one syntax error to test yippie_err.py
+
+import sys
+
+def one(:
+    bleh = 1
+
+    arr[3] = [1, 2, 3]
+    arr[0] += bleh
+
+    print(bleh)
+
+
+if __name__ == "__main__":
+    one()
--- yippie_err.py
+++ yippie_err.py
@@ -1,1 +1,22 @@
-# A tool to make errors much more evident
+# A tool to make errors in code much more evident
+
+import ast
+import sys
+
+def check_syntax(filename):
+    with open(filename, 'r') as file:
+        code = file.read()
+
+    try:
+        ast.parse(code)
+        print(f"{filename} has no syntax errors")
+    except SyntaxError as e:
+        print(f"Syntax error on line {e.lineno}: {e.msg}")
+        print(f"Text: {e.text}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python yippie_err.py <filename>")
+        sys.exit(1)
+
+    check_syntax(sys.argv[1])
